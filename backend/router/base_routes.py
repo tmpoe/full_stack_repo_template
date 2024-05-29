5c10976cc2f4073de07ef6695167d4a6de1717c4
Matyas File <matyas.file@zeiss.com>
2024-05-29 08:46:01 +0200

    change content

--- backend/router/base_routes.py
+++ backend/router/base_routes.py
@@ -8,6 +8,6 @@ def routes() -> APIRouter:
     @router.get("/test")
     async def read_root() -> None:
         """Define example route."""
-        return {"Hello": "World"}
+        return {"Hello": "World - 2nd version"}
 
     return router
