efbd97ad8f4332dc6fb43cd6445d103eb1b337c4
tmpoe <torok.mark.patrik@gmail.com>
2024-11-26 11:29:04 +0100

    Add middleware example

--- products/product_name/backend/service_name/app.py
+++ products/product_name/backend/service_name/app.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from controller.example_routes import example_routes
 from controller.user_routes import user_routes
+from service_name.controller.middleware import LoggingMiddleware
 
 app = FastAPI()
+
+app.add_middleware(BaseHTTPMiddleware, LoggingMiddleware)
 
 app.include_router(example_routes())
 app.include_router(user_routes())
