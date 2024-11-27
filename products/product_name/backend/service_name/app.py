414b613f6607415af651c8e3c0a5f96f13c99f1c
tmpoe <torok.mark.patrik@gmail.com>
2024-11-27 12:47:37 +0100

    Fix middleware

--- products/product_name/backend/service_name/app.py
+++ products/product_name/backend/service_name/app.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
-from starlette.middleware.base import BaseHTTPMiddleware
 
 from service_name.controller.example_routes import example_routes
 from service_name.controller.user_routes import user_routes
 from service_name.controller.authentication.auth_routes import auth_routes
-from service_name.controller.middleware import LoggingMiddleware
+from service_name.controller.middleware.LoggingMiddleware import LoggingMiddleware
 
 app = FastAPI()
 
-app.add_middleware(BaseHTTPMiddleware, LoggingMiddleware)
+app.add_middleware(LoggingMiddleware)
 
 app.include_router(example_routes())
 app.include_router(user_routes())
